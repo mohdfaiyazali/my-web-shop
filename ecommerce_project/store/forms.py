732dc1e54d411e58d102bd4ca59128b775a5dfab
mohdfaiyazali <alfaiyaz561@gmail.com>
2026-04-27 21:41:28 +0530

    fixed project structure for render

--- ecommerce_project/store/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Product, Cart, CartItem
-
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = ['name', 'price', 'description','image','stock']
-
-
